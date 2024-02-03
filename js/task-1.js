5f3b858baabdf6459dd491252684245278d22fb7
INNA-KUSHNIRR1 <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2024-02-03 22:14:40 +0100

    4

--- js/task-1.js
+++ js/task-1.js
@@ -2,10 +2,12 @@ const login = prompt(`CIAO!   CHI SEI???   🧐 🧐 🧐 🧐 🧐`);
 
 function checkLoginPassword(login) {
   if (login === 'zaraza') {
-    const password = prompt('MENOMALE SAI CHI SONO!     ALLORA PASSWORD  😁🤪');
+    const password = prompt(
+      'MENOMALE SAI CHI SONO!             ALLORA PASSWORD  😁🤪'
+    );
     if (password === 'ti amo lipuchka') {
       alert(
-        '💋💋 💋💋 💋💋 💋💋 😘😘 😘😘 😘😘 😘😘 💋💋 💋💋 💋💋 💋💋😘😘 😘😘 😘😘 😘😘'
+        '💋💋 💋💋 💋💋 💋💋 😘😘 😘😘 😘😘 😘😘 😘😘 😘😘 😘😘 💋💋 💋💋 💋💋 💋💋 '
       );
     } else {
       alert('😭 😭 😭 😭 😭 💔 💔 💔');
